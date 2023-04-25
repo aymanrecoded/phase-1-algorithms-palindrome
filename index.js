c7a3aadf695a2fd92bb1903888ffc04d23ca7b6f
aymanattar <aymanxman19999@gmail.com>
2023-04-25 08:34:20 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let newWord = word.toLowerCase();
+  let newWordRevevse = newWord.split("").reverse().join("");
+  return newWord === newWordRevevse;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
